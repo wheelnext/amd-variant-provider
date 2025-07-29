3ec4a3849cb3f0cdafc26b3d676b7d0ddbea0b03
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2025-07-29 17:11:11 -0500

    Update plugin.py
    Using https://github.com/wheelnext/wheelnext/blob/main/docs/proposals/pepxxx_wheel_variant_support.md#plugin-api-1 as reference

--- amd_variant_provider/plugin.py
+++ amd_variant_provider/plugin.py
@@ -3,8 +3,8 @@ from variantlib.models.provider import VariantFeatureConfig
 class AMDVariantPlugin:
     namespace = "amd"
 
-    def get_all_configs(self) -> list[VariantFeatureConfig]:
+    def validate_property(self, variant_property: VariantPropertyType) -> bool:
         pass
 
-    def get_supported_configs(self) -> list[VariantFeatureConfig]:
+    def get_supported_configs(self, known_properties: frozenset[VariantPropertyType] | None) -> list[VariantFeatureConfigType]:
         pass
